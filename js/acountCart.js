fd85c101f331ace5bfa5ee6d19c910b2c36be946
unknown <caballerosuarezsantiago@gmail.com>
2022-05-16 23:09:43 -0500

    chck ready

--- js/acountCart.js
+++ js/acountCart.js
@@ -14,7 +14,10 @@ if ((window.location.href === 'http://localhost/paginaTours/tours/shop.html') ||
     window.onload = read;
 }
 
+read.ready = verifyContentStorage;
+
 function read() {
+    console.log("read");
     let show = '';
     fetch(URL)
         .then(response => response.json())
@@ -32,10 +35,13 @@ function read() {
                       <div id="${title}"></div>
                     </div>
                     <h4>$ ${newPrice}</h4>
-                    <button id="add" onclick="add('${id}')" class="button-add">Agregar</button>
+                    <button id="add${id}" onclick="add('${id}')" class="button-add">Agregar</button>
+                    <button style="visibility: hidden" id="${id}" onclick="deleteItem('${id}')" class="button-add btn btn-danger">Quitar</button>
                 </div>`
                 }
                 document.getElementById("content_product").innerHTML = show;
+                verifyContentStorage();
+                btnPay();
             } catch (error) {
                 console.log(error);
             }
@@ -109,6 +115,7 @@ function add(id) {
     totalSum = new Intl.NumberFormat('es-ES').format(total);
     document.getElementById("checkoutB").innerHTML = `Pagar $${totalSum}`;
     btnPay();
+    verifyContentStorage();
 }
 
 function btnPay() {
@@ -127,8 +134,57 @@ function btnPay() {
     }
 
     if (totalPrice > 0) {
+        console.log("entro en btn");
         b.style.visibility = "visible";
         totalPrice = new Intl.NumberFormat('es-ES').format(totalPrice);
         b.innerHTML = `Pagar $${totalPrice}`;
     }
+    if (totalPrice == 0) {
+        b.style.visibility = "hidden";
+    }
+}
+
+function verifyContentStorage() {
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        value = JSON.parse(value);
+        if (value.length > 0) {
+            for (let j = 0; j < value.length; j++) {
+                value[j] = JSON.parse(value[j]);
+                value[j].map(tour => {
+                    const { id } = tour;
+                    const idBtn = "add" + id;
+                    console.log(idBtn);
+                    document.getElementById(idBtn).style.visibility = "hidden";
+                    document.getElementById(id).style.visibility = "visible";
+                    document.getElementById(id).style.position = "absolute";
+                })
+            }
+        }
+    }
+}
+
+function deleteItem(id) {
+        let dataTotal = [];
+        let invoices = JSON.parse(localStorage.getItem("Invoices"));
+        let idCart = id;
+        invoices.map(function (item) {
+            item = JSON.parse(item);
+            for (let i = 0; i < item.length; i++) {
+                if (item[i].id == idCart) {
+                    item.splice(i, 1);
+                }
+            }
+            dataTotal.push(JSON.stringify(item));
+            for (let i = 0; i < dataTotal.length; i++) {
+                if (dataTotal[i] === "[]" || dataTotal[i] === null) {
+                    dataTotal.splice(i, 1);
+                }
+            }
+            console.log(dataTotal);
+        });
+        localStorage.setItem("Invoices", JSON.stringify(dataTotal));
+        read();
+        verifyContentStorage();
 }
