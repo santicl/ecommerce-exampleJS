18d0836d55cacb6fd09c17ad1126621a6ab4e908
santiago <caballerosuarezsantiago@gmail.com>
2022-08-17 16:04:52 -0500

    add new file

--- js/acountCart.js
+++ js/acountCart.js
@@ -1,26 +1,26 @@
 import { showCarts, getDataTour, btnPay, sumTotal, verifyBtnRemove } from "./components/components.js";
-import { arrayBtnItems } from "./components/components.js";
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
-//import { getDatabase } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+import { getDatabase, ref, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-database.js";
 
-// CONFIG FIREBASE
-//const firebaseConfig = {
-//    apiKey: "AIzaSyAYzSPpwPVcQRyv7J7zNbGCFFW_QUc6cKA",
-//    authDomain: "bd-api-data.firebaseapp.com",
-//    databaseURL: "https://bd-api-data-default-rtdb.firebaseio.com",
-//    projectId: "bd-api-data",
-//    storageBucket: "bd-api-data.appspot.com",
-//    messagingSenderId: "137490408138",
-//    appId: "1:137490408138:web:7a05a9d61728c712bbca33",
-//    measurementId: "G-HS1VJ4SR1G"
-//};
+const firebaseConfig = {
+    apiKey: "AIzaSyD0lXJwJCWfoyyMeQm8I5CS83qFsqaAU-0",
+    authDomain: "db-checkout.firebaseapp.com",
+    databaseURL: "https://db-checkout-default-rtdb.firebaseio.com",
+    projectId: "db-checkout",
+    storageBucket: "db-checkout.appspot.com",
+    messagingSenderId: "554449152349",
+    appId: "1:554449152349:web:97acf721cbb9609526e0b4",
+    measurementId: "G-F2TNB7KC4F"
+};
 
-//Firebase
-//const app = initializeApp(firebaseConfig);
-//const db = getDatabase(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
 
 // API TOURS
 const URL = 'https://api-tours-default-rtdb.firebaseio.com/tours.json';
+const URL_DB_CHECKOUT = 'https://db-checkout-default-rtdb.firebaseio.com/';
 
 let totalSum = 0, total = 0, totalCarts = [];
 var cart;
@@ -90,6 +90,7 @@ function add(id) {
         if (totalCarts[i].id == cart) {
             dataCarts.push(totalCarts[i]);
             dataCarts = JSON.stringify(dataCarts);
+            insertDB(totalCarts[i], totalCarts[i].id);
         }
     }
 
@@ -112,4 +113,19 @@ function add(id) {
     totalSum = new Intl.NumberFormat('es-ES').format(total.suma);
     document.getElementById("checkoutB").innerHTML = `Pagar $${totalSum}`;
     verifyBtnRemove();
+}
+
+const insertDB = (data, id) => {
+    set(ref(db, 'tour/' + id), {
+        title: data.title,
+        price: data.price,
+        type: data.type,
+        include: data.include,
+        id: data.id,
+        rank: data.rank
+    });
+}
+
+const removeItem = (id) => {
+    remove(ref(db, 'tour/' + id));
 }
