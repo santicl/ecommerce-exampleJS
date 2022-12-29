dbb5314989b6cdb83a0225ffdccb83822f2061bf
santicl <caballerosuarezsantiago@gmail.com>
2022-12-29 11:12:41 -0500

    intrash files

--- js/acountCart.js
+++ js/acountCart.js
@@ -1,6 +1,5 @@
-import { showCarts, getDataTour, btnPay, sumTotal, verifyBtnRemove, getSumaAsync } from "./components/components.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
-import { getDatabase, ref, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-database.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD0lXJwJCWfoyyMeQm8I5CS83qFsqaAU-0",
@@ -19,162 +18,241 @@ const db = getDatabase(app);
 
 
 // API TOURS
-const URL = 'https://api-tours-default-rtdb.firebaseio.com/tours.json';
-const URL_DB_CHECKOUT = 'https://db-checkout-default-rtdb.firebaseio.com/tour.json';
+const URL_ = 'https://api-tours-default-rtdb.firebaseio.com/tours.json';
 
 let totalSum = 0, total = 0, totalCarts = [];
 var cart;
 
-if ((window.location.href === 'http://localhost/paginaTours/tours/') || (window.location.href === 'https://toursopen.netlify.app/' || window.location.href === 'https://toursopen.netlify.app/index.html' || window.location.href === 'http://localhost/paginaTours/tours/index.html')) {
+if ((window.location.href === 'http://localhost/paginaTours/tours/') || (window.location.href === 'https://toursopen.netlify.app/' || window.location.href === 'https://toursopen.netlify.app/index.html' || window.location.href === 'http://localhost/paginaTours/tours/index.html' || window.location.href === 'https://touropen.com.co/')) {
     window.onload = readOutsideOrInsideTour;
 }
 
 readOutsideOrInsideTour.onload = returnItemDescription;
-readOutsideOrInsideTour.onload = verifyBtnRemove;
 
-function readOutsideOrInsideTour() {
-    localStorage.clear();
-    let showOutside = '', showInside = '';
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            totalCarts = data;
-            try {
-                for (let i = 0; i < data.length; i++) {
-                    let { type, price } = data[i];
-                    let newPrice = new Intl.NumberFormat('es-ES').format(price);
-                    if (type === 'outside') {
-                        showOutside += showCarts(getDataTour(data[i]), newPrice);
-                    } else {
-                        showInside += showCarts(getDataTour(data[i]), newPrice);
-                    }
-                }
-                document.getElementById("content_product").innerHTML = showOutside;
-                document.getElementById("carousel__container_content_product").innerHTML = showInside;
-                verifyBtnRemove();
-            } catch (error) {
-                console.log(error);
-            }
-        })
-        .catch(error => console.log(error));
-    returnItemDescription();
-    btnPay();
+export const showCarts = (tour, newPrice) => {
+    console.log("showcarts");
+    return `<div id="div" class="product-container">
+    <h3>${tour.title}</h3>
+    <img src="img/${tour.img}" />
+    <div class="container-included">
+      <div id="${tour.title}"></div>
+    </div>
+    <h4>$ ${newPrice}</h4>
+    <div id="${tour.id}-${tour.title}">
+    <button class="button-add">
+    <a href='https://api.whatsapp.com/send?phone=573162421339&text=Hola%20TourOpen%20%F0%9F%98%84%F0%9F%98%84%3B%20estoy%20interesado%20en%20el%20tour%20${tour.title}'>Cotizar</a>
+    </button>
+    </div>
+</div>`;
 }
 
-function returnItemDescription() {
-    let container = '';
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                const { title, include } = data[i];
-                container = document.getElementById(title);
-                container.innerHTML = '';
-                for (let j = 0; j < include.length; j++) {
-                    container.innerHTML += `<li class="list"><i class="bi bi-check2-circle"></i> ${include[j]}</li><br>`;
-                }
-            }
-        })
-}
-
-document.getElementById("body-shop").addEventListener("click", function (e) {
-    if (e.target.classList.contains("button-add")) {
-        add(e.target.id);
-    }
-})
-
-function add(id) {
-    let dataCarts = [], data = [], b = document.getElementById("checkoutB");;
-    cart = id;
-    for (let i = 0; i < totalCarts.length; i++) {
-        if (totalCarts[i].id == cart) {
-            dataCarts.push(totalCarts[i]);
-            dataCarts = JSON.stringify(dataCarts);
-            insertDB(totalCarts[i], totalCarts[i].id);
+export const verifyBtnRemove = () => {
+    let btnState = true;
+    let dataContent = {};
+    let tour = JSON.parse(localStorage.getItem("Invoices"));
+    for (let i = 0; i < tour.length; i++) {
+        tour[i] = JSON.parse(tour[i]);
+        for (let j = 0; j < tour[i].length; j++) {
+            const { id, title } = tour[i][j];
+            dataContent = { btnState, id, title };
+            let IDChange = getIdTransform(dataContent);
+            console.log(document.getElementById(IDChange));
+            document.getElementById(IDChange).innerHTML = `<button id="${id}" class="button-remove">Quitar</button>`;
         }
     }
-
-    if (localStorage.getItem("Invoices") === null) {
-        let invoices = [];
-        invoices.push(dataCarts);
-        localStorage.setItem("Invoices", JSON.stringify(invoices));
-    } else {
-        let invoices = JSON.parse(localStorage.getItem("Invoices"));
-        invoices.push(dataCarts);
-        localStorage.setItem("Invoices", JSON.stringify(invoices));
-    }
-
-    total = sumTotal();
-
-    if (total.suma > 0) {
-        b.style.visibility = "visible";
-    }
-
-    totalSum = new Intl.NumberFormat('es-ES').format(total.suma);
-    document.getElementById("checkoutB").innerHTML = `Pagar $${totalSum}`;
-    verifyBtnRemove();
+    arrayElementsByBtn();
+    arrayBtnItems();
 }
 
-const insertDB = (data, id) => {
-    set(ref(db, 'tour/' + id), {
-        title: data.title,
-        price: data.price,
-        type: data.type,
-        include: data.include,
-        id: data.id,
-        rank: data.rank
+const arrayElementsByBtn = () => {
+    let btns = document.getElementsByClassName("button-remove");
+    for (const el of btns) {
+        el.addEventListener('click', (e) => {
+            modifyClassName(e.target.id);
+            deleteItem(e.target.id);
+        })
+    }
+}
+
+const getIdTransform = (dataContent) => {
+    let ID;
+    if (dataContent.btnState === true) {
+        ID = dataContent.id + "-" + dataContent.title;
+    }
+    if (dataContent.btnState === false) {
+        ID = dataContent.title + "-" + dataContent.id;
+    }
+    return ID;
+}
+
+const deleteItem = (ID) => {
+    console.log("delete");
+    let dataTotal = [];
+    let invoices = JSON.parse(localStorage.getItem("Invoices"));
+    invoices.map(function (item) {
+        item = JSON.parse(item);
+        for (let i = 0; i < item.length; i++) {
+            const { id, title } = item[i];
+            if (id == ID) {
+                console.log(ID);
+                item.splice(i, 1);
+            }
+        }
+        dataTotal.push(JSON.stringify(item));
+        for (let i = 0; i < dataTotal.length; i++) {
+            if (dataTotal[i] === "[]" || dataTotal[i] === null) {
+                dataTotal.splice(i, 1);
+            }
+        }
     });
+    localStorage.setItem("Invoices", JSON.stringify(dataTotal));
+    btnPay();
 }
 
-const removeItem = (id) => {
-    remove(ref(db, 'tour/' + id));
+const modifyClassName = (idBtn) => {
+    let tour = JSON.parse(localStorage.getItem("Invoices"));
+    for (let i = 0; i < tour.length; i++) {
+        tour[i] = JSON.parse(tour[i]);
+        console.log(tour[i]);
+        for (let j = 0; j < tour[i].length; j++) {
+            const { id, title } = tour[i][j];
+            if (id == idBtn) {
+                document.getElementById(id + "-" + title).innerHTML = `<button id="${id}" class="button-add">Agregar</button>`;
+            }
+        }
+    }
 }
 
-readOutsideOrInsideTour.onload = async (URL_DB_CHECKOUT) => {
-    let data = await getSumaAsync(URL_DB_CHECKOUT);
-    console.log(data);
+export const verifyContentStorage = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        value = JSON.parse(value);
+        if (value.length > 0) {
+            for (let j = 0; j < value.length; j++) {
+                value[j] = JSON.parse(value[j]);
+                value[j].map(tour => {
+                    const { id } = tour;
+                    const idBtn = id + "quip";
+                    document.getElementById(idBtn).style.visibility = "hidden";
+                    document.getElementById(id).style.visibility = "visible";
+                    document.getElementById(id).style.position = "absolute";
+                })
+            }
+        }
+    }
+}
+
+export const getDataTour = (value) => {
+    const { title, img, id } = value;
+    const tour = { title, img, id };
+    return tour;
+}
+
+export const sumTotal = () => { //aqui se puede hacer el descuento del 10%
+    let invoices = JSON.parse(localStorage.getItem("Invoices"));
+    let suma = 0, porcent = 0, sumAndPorcent = {};
+
+    for (let i = 0; i < invoices.length; i++) {
+        invoices[i] = JSON.parse(invoices[i]);
+        for (let j = 0; j < invoices[i].length; j++) {
+            invoices[i][j].price = parseInt(invoices[i][j].price);
+            suma += invoices[i][j].price;
+            porcent = suma * 0.19;
+        }
+        sumAndPorcent = { suma, porcent };
+    }
+    return sumAndPorcent;
+}
+
+export const getSumaAsync = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+export const btnPay = () => {
+    let totalPrice = 0;
+    let b = document.getElementById("checkoutB");
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        value = JSON.parse(value);
+        value.map(tour => {
+            tour = JSON.parse(tour);
+            for (let j = 0; j < tour.length; j++) {
+                totalPrice += tour[j].price;
+            }
+        })
+    }
+
+    if (totalPrice > 0) {
+        b.style.visibility = "visible";
+        totalPrice = new Intl.NumberFormat('es-ES').format(totalPrice);
+        b.innerHTML = `Pagar $${totalPrice}`;
+    }
+    if (totalPrice == 0) {
+        b.style.visibility = "hidden";
+    }
+}
+
+export const API_WapSend = () => {
+    let name = document.getElementById("name").value;
+    let date = document.getElementById("date-tour").value;
+    let hourLocal = document.getElementById("hour-local").value;
+    let numberPerson = document.getElementById("number-persons").value;
+    let tours = getTours();
+
+    const API = 'https://api.whatsapp.com/send?phone=573162421339&text=Hola%20%F0%9F%98%8A%2C%20mi%20nombre%20es%20' + name + '%20deseo%20reservar%20uno%20o%20varios%20tures%20como%20' + tours + '%2C%20en%20la%20hora%20y%20fecha%20' + hourLocal + ' ' + date + '%20%2C%20para%20' + numberPerson + '%2C%20quisiera%20mas%20informaci%C3%B3n';
+    window.location.href = API;
+}
+
+const getTours = () => {
+    let data = " ";
+    let tour = JSON.parse(localStorage.getItem("Invoices"));
+    for (let i = 0; i < tour.length; i++) {
+        tour[i] = JSON.parse(tour[i]);
+        for (let j = 0; j < tour[i].length; j++) {
+            const { title } = tour[i][j];
+            data += title + ", ";
+        }
+    }
+    return data;
+}
+
+const getItems = (data, idData) => {
+    let newData;
+    for (let i = 0; i < data.length; i++) {
+        data[i] = JSON.parse(data[i]);
+        for (let j = 0; j < data[i].length; j++) {
+            const { id } = data[i][j];
+            if (id === idData) {
+                newData = data[i][j];
+            }
+        }
+    }
+    return newData;
+}
+
+export const arrayBtnItems = () => {
+    let btnAdd = document.getElementsByClassName("button-add");
+    for (const el of btnAdd) {
+        el.addEventListener('click', (e) => {
+            // getID for set to API Firebase.
+            let tour = JSON.parse(localStorage.getItem("Invoices"));
+            let getTour = getItems(tour, e.target.id);
+            console.log(getTour);
+        })
+    }
 }
 
 
-/*
-import { showCarts, getDataTour, btnPay, sumTotal, verifyBtnRemove } from "./components/components.js";
-import { arrayBtnItems } from "./components/components.js";
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
-//import { getDatabase } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js";
 
-// CONFIG FIREBASE
-//const firebaseConfig = {
-//    apiKey: "AIzaSyAYzSPpwPVcQRyv7J7zNbGCFFW_QUc6cKA",
-//    authDomain: "bd-api-data.firebaseapp.com",
-//    databaseURL: "https://bd-api-data-default-rtdb.firebaseio.com",
-//    projectId: "bd-api-data",
-//    storageBucket: "bd-api-data.appspot.com",
-//    messagingSenderId: "137490408138",
-//    appId: "1:137490408138:web:7a05a9d61728c712bbca33",
-//    measurementId: "G-HS1VJ4SR1G"
-//};
-
-//Firebase
-//const app = initializeApp(firebaseConfig);
-//const db = getDatabase(app);
-
-// API TOURS
-const URL = 'https://api-tours-default-rtdb.firebaseio.com/tours.json';
-
-let totalSum = 0, total = 0, totalCarts = [];
-var cart;
-
-if ((window.location.href === 'http://localhost/paginaTours/tours/') || (window.location.href === 'https://toursopen.netlify.app/' || window.location.href === 'https://toursopen.netlify.app/index.html' || window.location.href === 'http://localhost/paginaTours/tours/index.html')) {
-    window.onload = readOutsideOrInsideTour;
-}
-
-readOutsideOrInsideTour.onload = returnItemDescription;
-readOutsideOrInsideTour.onload = verifyBtnRemove;
 
 function readOutsideOrInsideTour() {
     localStorage.clear();
     let showOutside = '', showInside = '';
-    fetch(URL)
+    fetch(URL_)
         .then(response => response.json())
         .then(data => {
             totalCarts = data;
@@ -190,19 +268,17 @@ function readOutsideOrInsideTour() {
                 }
                 document.getElementById("content_product").innerHTML = showOutside;
                 document.getElementById("carousel__container_content_product").innerHTML = showInside;
-                verifyBtnRemove();
             } catch (error) {
                 console.log(error);
             }
         })
         .catch(error => console.log(error));
     returnItemDescription();
-    btnPay();
 }
 
 function returnItemDescription() {
     let container = '';
-    fetch(URL)
+    fetch(URL_)
         .then(response => response.json())
         .then(data => {
             for (let i = 0; i < data.length; i++) {
@@ -215,41 +291,3 @@ function returnItemDescription() {
             }
         })
 }
-
-document.getElementById("body-shop").addEventListener("click", function (e) {
-    if (e.target.classList.contains("button-add")) {
-        add(e.target.id);
-    }
-})
-
-function add(id) {
-    let dataCarts = [], data = [], b = document.getElementById("checkoutB");;
-    cart = id;
-    for (let i = 0; i < totalCarts.length; i++) {
-        if (totalCarts[i].id == cart) {
-            dataCarts.push(totalCarts[i]);
-            dataCarts = JSON.stringify(dataCarts);
-        }
-    }
-
-    if (localStorage.getItem("Invoices") === null) {
-        let invoices = [];
-        invoices.push(dataCarts);
-        localStorage.setItem("Invoices", JSON.stringify(invoices));
-    } else {
-        let invoices = JSON.parse(localStorage.getItem("Invoices"));
-        invoices.push(dataCarts);
-        localStorage.setItem("Invoices", JSON.stringify(invoices));
-    }
-
-    total = sumTotal();
-
-    if (total.suma > 0) {
-        b.style.visibility = "visible";
-    }
-
-    totalSum = new Intl.NumberFormat('es-ES').format(total.suma);
-    document.getElementById("checkoutB").innerHTML = `Pagar $${totalSum}`;
-    verifyBtnRemove();
-}
-*/
